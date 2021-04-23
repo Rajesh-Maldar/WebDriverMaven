f0f33f496a550168da27833762ed0925d6396a98
rajesh <rajeshmaldar@gmail.com>
2021-04-23 14:13:39 +0530

    Comitting the changes made to LoginTest

--- src/test/java/webdriverMaven/LoginTest.java
+++ src/test/java/webdriverMaven/LoginTest.java
@@ -24,7 +24,7 @@ public class LoginTest {
 	public void doLogin() {
 		driver.get("https://accounts.google.com/signin/v2/identifier?continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&service=mail&sacu=1&rip=1&flowName=GlifWebSignIn&flowEntry=ServiceLogin");
 		driver.findElement(By.id("identifierId")).sendKeys("rajeshmaldar@gmail.com");
-		//driver.findElement(By.id("Password")).sendKeys("sdfas");
+		driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div/div[1]/div/div/button")).click();
 		
 	}
 	
